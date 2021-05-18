885df4c074f0333012df2bad790475f3161644f8
Arthur Telders <atelders@gmail.com>
2021-05-18 14:28:29 +0200

    Update

--- src/main.py
+++ src/main.py
@@ -3,15 +3,44 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-DATA_URL = ('../data/raw/Emotion_final.csv')
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
+
+data_input = st.sidebar.radio('Data', ['Kaggle','data.world'])
 
 @st.cache
 def load_data():
-    data = pd.read_csv(DATA_URL)
+    if data_input == 'Kaggle':
+        DATA_URL = ('../data/raw/Emotion_final.csv')
+        data = pd.read_csv(DATA_URL)
+    elif data_input == 'data.world':
+        DATA_URL = ('../data/raw/text_emotion.csv')
+        data = pd.read_csv(DATA_URL)
+        data['emotion'] = data['sentiment']
+        data['text'] = data['content']
+        data = data[['text', 'emotion']]
+    else:
+        st.write('No data')
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     return data
+
+
+@st.cache(allow_output_mutation=True)
+def create_model(data, model):
+    if model == 'TfidfVectorizer':
+        tf = TfidfVectorizer(analyzer = 'word', ngram_range = (1, 2), min_df = 0, stop_words = {'english'})
+    elif model == 'CountVectorizer':
+        tf = CountVectorizer(analyzer = 'word', ngram_range = (1, 2), min_df = 0, stop_words = {'english'})   
+    tfidf_matrix = tf.fit_transform(data)
+    return tf, tfidf_matrix
 
 st.title('Emotion recognition')
 
@@ -19,10 +48,27 @@ st.title('Emotion recognition')
 data = load_data()
 
 
+le = preprocessing.LabelEncoder()
+y = le.fit_transform(data['emotion'])
+
+sentences = data['text'].values
+
+sentences_train, sentences_test, y_train, y_test = train_test_split(
+   sentences, y, test_size=0.25, random_state=1000)
+
+
+
+vectorizer = CountVectorizer()
+vectorizer.fit(sentences_train)
+
+X_train = vectorizer.transform(sentences_train)
+X_test  = vectorizer.transform(sentences_test)
+
 chapter_input = st.sidebar.radio('Chapters', ['Analysis and processing','Classification'])
 
 if chapter_input == 'Analysis and processing':
-    st.dataframe(data.head(10))
+    if st.checkbox('Show dataframe'): 
+        st.dataframe(data.head(10))
 
     results = data['emotion'].value_counts()
     sns.histplot(data=data, y="emotion")
@@ -32,18 +78,31 @@ if chapter_input == 'Analysis and processing':
 
 if chapter_input == 'Classification':
     st.write('Classification models')
+    alg=['Decision Tree', 'Random Forest', 'Support Vector Machine']
+    y_label = le.inverse_transform(y_test)
 
-    st.button('Hit me')
-    st.checkbox('Check me out')
-    st.radio('Radio', [1,2,3])
-    st.selectbox('Select', [1,2,3])
-    st.multiselect('Multiselect', [1,2,3])
-    st.slider('Slide me', min_value=0, max_value=10)
-    st.select_slider('Slide to select', options=[1,'2'])
-    st.text_input('Enter some text')
-    st.number_input('Enter a number')
-    st.text_area('Area for textual entry')
-    st.date_input('Date input')
-    st.time_input('Time entry')
-    st.file_uploader('File uploader')
-    st.color_picker('Pick a color')
+    classifier = st.selectbox('Which algorithm?', alg)
+    if classifier=='Decision Tree':
+        dtc = DecisionTreeClassifier()
+        dtc.fit(X_train, y_train)
+        acc = dtc.score(X_test, y_test)
+        st.write('Accuracy: ', acc)
+        pred_dtc = dtc.predict(X_test)
+        cm_dtc=confusion_matrix(y_test,pred_dtc)
+        st.write('Confusion matrix: ', cm_dtc)
+    elif classifier == 'Random Forest':
+        clf = RandomForestClassifier(random_state=0)
+        clf.fit(X_train, y_train)
+        acc = clf.score(X_test, y_test)
+        st.write('Accuracy: ', acc)
+        pred_clf = clf.predict(X_test)
+        cm_clf=confusion_matrix(y_test,pred_clf)
+        st.write('Confusion matrix: ', cm_clf)        
+    elif classifier == 'Support Vector Machine':
+        svm=SVC()
+        svm.fit(X_train, y_train)
+        acc = svm.score(X_test, y_test)
+        st.write('Accuracy: ', acc)
+        pred_svm = svm.predict(X_test)
+        cm=confusion_matrix(y_test,pred_svm)
+        st.write('Confusion matrix: ', cm)
